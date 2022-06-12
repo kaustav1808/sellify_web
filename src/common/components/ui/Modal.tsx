53d712e9bc098ed26ee42190f84039be211b9875
KAUSTAV BHATTACHARYA <kaustav.bhattacharya@codelogicx.com>
2022-06-12 16:32:20 +0530

    lint fix.

--- src/common/components/ui/Modal.tsx
+++ src/common/components/ui/Modal.tsx
@@ -4,17 +4,17 @@ import { ReactElement } from 'react';
 type ModalType = {
   show: Boolean;
   resetModal: Function;
-  children:ReactElement
+  children: ReactElement;
 };
 
-const Modal: NextPage = ({show=false,resetModal, children}: ModalType) => {
+const Modal: NextPage = ({ show = false, resetModal, children }: ModalType) => {
   return (
     <div className={`modal  ${show ? 'modal-open' : ''}`}>
       <div className="modal-box w-10/12 bg-base-200">
         <label
           htmlFor="my-modal-3"
           onClick={() => {
-           resetModal(false);
+            resetModal(false);
           }}
           className="btn btn-sm btn-circle absolute right-2 top-2"
         >
@@ -26,4 +26,4 @@ const Modal: NextPage = ({show=false,resetModal, children}: ModalType) => {
   );
 };
 
-export default Modal
+export default Modal;
