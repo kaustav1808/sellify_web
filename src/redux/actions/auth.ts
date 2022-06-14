51266003eca582cd086feb1e61653aa63bf49b26
KAUSTAV BHATTACHARYA <kaustav.bhattacharya@codelogicx.com>
2022-06-14 20:48:55 +0530

    linting fix.

--- src/redux/actions/auth.ts
+++ src/redux/actions/auth.ts
@@ -3,14 +3,12 @@ import client from 'src/api/client';
 
 export const signUp =
   (payload = {}) =>
-  (dispatch: any) =>{
-    client.post('/auth/signup', payload)
-    .then(res=>{
+  (dispatch: any) => {
+    client.post('/auth/signup', payload).then((res) => {
       console.log(res);
       dispatch({
         type: t.SIGN_UP,
         payload: res.data.user,
       });
-    })
-  }
-    
+    });
+  };
