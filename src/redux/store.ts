ac4397551705212777a557379a138cf1d586fba4
KAUSTAV BHATTACHARYA <kaustavofficial1808@gmail.com>
2022-07-01 14:19:13 +0530

    update build process and fix configureStore.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,9 +6,6 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 export const store = () =>
-  configureStore(
-    { reducer: rootReducer },
-    composeWithDevTools(applyMiddleware([thunk])),
-  );
+  configureStore({ reducer: rootReducer, middleware: [thunk], devTools: true });
 
 export const wrapper = createWrapper(store);
