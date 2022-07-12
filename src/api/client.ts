0852aae68320984ca05ae3df9d0624a18e7c1026
kaustav bhattacharya <kaustavofficial1808@gmail.com>
2022-07-12 16:19:01 +0530

    fix lint

--- src/api/client.ts
+++ src/api/client.ts
@@ -5,7 +5,7 @@ axios.defaults.baseURL = process.env.NEXT_PUBLIC_CLIENT_URL;
 axios.defaults.headers.common.Accept = 'application/json';
 axios.defaults.timeout = 12000;
 
-console.log(axios.defaults)
+console.log(axios.defaults);
 
 const getHttpHeaders = (): AxiosRequestConfig => {
   const authToken = getAuthToken();
