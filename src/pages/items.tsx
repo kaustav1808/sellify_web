d4075dc2f4ecb9e96c791f3b5036a8b36236c56b
kaustav bhattacharya <kaustavofficial1808@gmail.com>
2022-07-30 21:34:31 +0530

    add items page.

--- src/pages/items.tsx
+++ src/pages/items.tsx
@@ -8,8 +8,27 @@ type NextPageWithLayout = NextPage & {
 
 const Items: NextPageWithLayout = () => {
   return (
-    <div className="w-full h-inherit p-2">
-      <div className=""></div>
+    <div className="w-full h-inherit p-4">
+      <div className="flex">
+        <div className="tabs w-11/12">
+          <a className="tab tab-lg tab-bordered">All</a>
+          <a className="tab tab-lg tab-bordered tab-active">Setteled</a>
+          <a className="tab tab-lg tab-bordered">Closed</a>
+        </div>
+        <div className="w-1/12">
+          <button className="btn btn-active btn-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z" />
+            </svg>
+            Add Item
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
