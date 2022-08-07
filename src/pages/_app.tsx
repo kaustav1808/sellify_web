13703a805a1cf7f785f3dd40e9b5b6c306ded47f
kaustav bhattacharya <kaustavofficial1808@gmail.com>
2022-08-07 19:08:05 +0530

    prettify.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,9 @@ import { wrapper } from '../redux/store';
 import { makeServer } from '../mocks/mirage';
 
 if (process.env.NEXT_PUBLIC_DEMO_ENV === 'true') {
-  console.log("Starting Mocking server...!!!")
+  console.log('Starting Mocking server...!!!');
   makeServer();
 }
-
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
