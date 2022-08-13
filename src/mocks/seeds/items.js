1630e8e60b5d9c5bdae36a91a4894f5b5ea42dc7
kaustav bhattacharya <kaustavofficial1808@gmail.com>
2022-08-13 18:35:01 +0530

    prettify.

--- src/mocks/seeds/items.js
+++ src/mocks/seeds/items.js
@@ -1,25 +1,28 @@
 import { faker } from '@faker-js/faker';
 
 const run = (server) => {
-    console.log('Running items seed');
-    for (let i = 0; i < 15; i++) {
-      let sellType = faker.helpers.arrayElement(['fixed', 'range', 'auction'])
-      let priceMin = faker.datatype.number({ min: 100, max: 1000 })
-      let priceMax = faker.datatype.number({ min: priceMin, max: 10000 })
-      let item = {
-        id: faker.database.mongodbObjectId(),
-        title: faker.commerce.productName(),
-        shortDescription: faker.commerce.productDescription(),
-        description: faker.lorem.sentences(10),
-        tags: faker.helpers.uniqueArray(() => faker.commerce.productAdjective(), 10),
-        status: faker.helpers.arrayElement(['setteled', 'open']),
-        sellType,
-        priceMin,
-        priceMax,
-      };
-      server.create('item',item);
-    }
-    console.log('Completed items seed');
-  };
+  console.log('Running items seed');
+  for (let i = 0; i < 15; i++) {
+    let sellType = faker.helpers.arrayElement(['fixed', 'range', 'auction']);
+    let priceMin = faker.datatype.number({ min: 100, max: 1000 });
+    let priceMax = faker.datatype.number({ min: priceMin, max: 10000 });
+    let item = {
+      id: faker.database.mongodbObjectId(),
+      title: faker.commerce.productName(),
+      shortDescription: faker.commerce.productDescription(),
+      description: faker.lorem.sentences(10),
+      tags: faker.helpers.uniqueArray(
+        () => faker.commerce.productAdjective(),
+        10,
+      ),
+      status: faker.helpers.arrayElement(['setteled', 'open']),
+      sellType,
+      priceMin,
+      priceMax,
+    };
+    server.create('item', item);
+  }
+  console.log('Completed items seed');
+};
 
 export default run;
