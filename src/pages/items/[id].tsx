7c07de7a2d9363a7c8110e04dc23f801e8c7a8e8
kaustav bhattacharya <kaustavofficial1808@gmail.com>
2022-10-02 13:19:57 +0530

    setup item page and integrate react-image-gallery.

--- src/pages/items/[id].tsx
+++ src/pages/items/[id].tsx
@@ -2,6 +2,7 @@ import Main from '@components/layouts/Main';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { ReactElement, ReactNode } from 'react';
+import ImageGallery from 'react-image-gallery';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -10,7 +11,36 @@ type NextPageWithLayout = NextPage & {
 const Item: NextPageWithLayout = () => {
   const router = useRouter();
   const { id } = router.query;
-  return <div>Item : {id}</div>;
+  const images = [
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1019/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+  ];
+  return (
+    <div className="flex flex-col gap-1 h-full">
+      <div className="p-4 flex gap-2 w-full h-2/3">
+        <div className="w-1/2 max-h-fit h-full">
+          <ImageGallery items={images} />
+        </div>
+        <div className="w-1/2 max-h-fit h-full bg-slate-300"></div>
+      </div>
+      <div className="p-4 flex w-full gap-1 h-1/3">
+        <div className="w-1/4 max-h-fit h-full bg-slate-300"></div>
+        <div className="w-1/4 max-h-fit h-full bg-slate-300"></div>
+        <div className="w-1/4 max-h-fit h-full bg-slate-300"></div>
+        <div className="w-1/4 max-h-fit h-full bg-slate-300"></div>
+      </div>
+    </div>
+  );
 };
 
 Item.getLayout = function getLayout(page: ReactElement) {
