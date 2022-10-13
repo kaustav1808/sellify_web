5b7ae7f6b3462881332115bdac25bf9b75e57aa5
kaustav bhattacharya <kaustavofficial1808@gmail.com>
2022-10-13 22:54:34 +0530

    setup bid card in item page.

--- src/pages/items/[id].tsx
+++ src/pages/items/[id].tsx
@@ -32,6 +32,52 @@ const Item: NextPageWithLayout = () => {
     };
   }, [router.query]);
 
+  const getShortBids = () => {
+    return [1, 2, 3, 4].map((o) => {
+      if (o == 4) {
+        return (
+          <div key={o} className="max-h-fit h-64 ">
+            <div className="card w-full bg-base-100 shadow-xl image-full">
+              <figure>
+                <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <div>
+                  <h6 className="text-6xl text-center align-middle">More +</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div key={o} className="max-h-fit h-64 hover:cursor-pointer">
+            <div className="card w-full bg-base-100 shadow-xl image-full">
+              <figure>
+                <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <div className="flex gap-2">
+                  <div className="avatar">
+                    <div className="w-24 rounded ring ring-success ring-offset-base-100 ring-offset-2">
+                      <img src="https://placeimg.com/192/192/people" />
+                    </div>
+                  </div>
+                  <div>
+                    <h2 className="card-title text-slate-100">
+                      Kaustav Bhattacharya
+                    </h2>
+                    <p className="text-slate-100">(+91)........64</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
+    });
+  };
+
   const images = () =>
     [1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => {
       let id = Math.floor(Math.random() * 11) + 10 + 1000;
@@ -67,10 +113,9 @@ const Item: NextPageWithLayout = () => {
           </div>
         </div>
       </div>
-      <div className="max-h-fit h-48 bg-slate-300"></div>
-      <div className="max-h-fit h-48 bg-slate-300"></div>
-      <div className="max-h-fit h-48 bg-slate-300"></div>
-      <div className="max-h-fit h-48 bg-slate-300"></div>
+      <div className="col-span-4">
+        <div className="grid grid-cols-4 gap-2">{getShortBids()}</div>
+      </div>
     </div>
   );
 };
